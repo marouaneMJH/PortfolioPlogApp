6f83cfffe69eadb78ac74a97b67988a04f1a7142
Marouane El Moujahid <marwanmoujahid008@gmail.com>
2025-03-28 05:51:09 +0000

    set modefy LinkButton

--- front-end/src/components/basic/LinkButton.tsx
+++ front-end/src/components/basic/LinkButton.tsx
@@ -1,18 +1,39 @@
 import { NavLink } from "react-router-dom";
 
 import styled from "styled-components";
+interface LinkButtonProps {
+    children?: React.ReactNode;
 
-const LinkButton = styled(NavLink)`
+    $maxWidth?: string;
+    $minWidth?: string;
+
+    $height?: string;
+    $maxHeight?: string;
+    $minHeight?: string;
+
+    $padding?: string;
+    $margin?: string;
+}
+
+const LinkButton = styled(NavLink)<LinkButtonProps>`
     flex: 1;
     display: flex;
     align-items: center;
     justify-content: center;
     padding: 12px;
     border-radius: 8px;
-    min-width: 12rem;
+    height: ${(props) => props.$height || "2rem"};
+    max-height: ${(props) => props.$maxHeight || "2rem"};
+    min-height: ${(props) => props.$minHeight || "3rem"};
+
     text-decoration: none;
     font-weight: 500;
     transition: all 0.3s ease;
+
+    min-width: ${(props) => props.$minWidth || "12rem"};
+    margin: ${(props) => props.$margin || "0 auto"};
+    padding: ${(props) => props.$padding || "0 1rem"};
+    box-sizing: border-box;
 
     background-color: var(--bg-secondary-color);
     color: var(--main-color);
